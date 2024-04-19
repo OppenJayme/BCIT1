0ee6515fb759ba0d877a27ffb7ac519324fc6d05
Kyer Ivan <156319128+OppenJayme@users.noreply.github.com>
2024-04-19 23:55:14 +0800

    Add files via upload

---
+++ Web Dev/javascrip/practice/array/array.js
@@ -0,0 +1,9 @@
+let favSingers = ["John Cena", "Olivia Rodrigo", "Taylor Switft"];
+console.log(favSingers[0]);
+let favNumbers = [1, 2, 3, 4];
+let mixedArr = ["string", ["otherarray"],123, true];
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1][0]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
