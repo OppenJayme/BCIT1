1b87f3008b33a46656358cc733bc0aa6085334e3
Kyer Ivan <156319128+OppenJayme@users.noreply.github.com>
2024-10-14 22:42:37 +0800

    print a - z characters

---
+++ Prog 1/atoz.c
@@ -0,0 +1,21 @@
+/**
+ * C program to print all alphabets from a to z
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Alphabets from a - z are: \n");
+    for(ch='a'; ch<='z'; ch++)
+    {
+        printf("%c\n", ch);
+    }
+
+    return 0;
+}
+
+//97-122 if ascii
+// a = 97
